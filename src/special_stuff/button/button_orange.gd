8f60a14d8ca0cc6839d5d4b0f7719ab7bc3f123c
LIEKAR\xyh123 <2160774260@qq.com>
2025-12-13 22:14:00 +0800

    Level2 NB版

--- src/special_stuff/button/button_orange.gd
+++ src/special_stuff/button/button_orange.gd
@@ -1,15 +1,15 @@
 extends AnimatedSprite2D
-signal Charactor_pess_button_blue
-signal Charactor_pop_button_blue
+signal Charactor_pess_button_orange
+signal Charactor_pop_button_orange
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player or body is Phantom or body is Phantom_dev or body is Player_dev:
-		print_rich("[color=blue]💡 %s 触发了蓝色按钮！[/color]" % body.name)
+		print_rich("[color=orange]💡 %s 触发了橙色按钮！[/color]" % body.name)
 		self.play("press")
-		Charactor_pess_button_blue.emit()
+		Charactor_pess_button_orange.emit()
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body is Player or body is Phantom or body is Phantom_dev or body is Player_dev:
-		print_rich("[color=blue]💡 %s 离开了了蓝色按钮！[/color]" % body.name)
+		print_rich("[color=orange]💡 %s 离开了了橙色按钮！[/color]" % body.name)
 		self.play("popup")
-		Charactor_pop_button_blue.emit()
+		Charactor_pop_button_orange.emit()
